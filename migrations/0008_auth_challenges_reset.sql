ec4442f9f1357192d4cd4b9431f733426feab7d5
nh8030954-art <nh8030954@gmail.com>
2026-09-23 10:26:01 +0300

    Finalize homepage branding and city/search fixes (migrations/0008_auth_challenges_reset.sql)

--- migrations/0008_auth_challenges_reset.sql
+++ migrations/0008_auth_challenges_reset.sql
@@ -11,5 +11,5 @@ SELECT token_hash,user_id,purpose,expires_at,created_at FROM auth_challenges;
 DROP TABLE auth_challenges;
 ALTER TABLE auth_challenges_next RENAME TO auth_challenges;
 CREATE INDEX auth_challenges_expiry_idx ON auth_challenges(expires_at);
-UPDATE site_settings SET logo_url='/gmach-berega-mark.jpg', tagline='גדולה גמילות חסדים יותר מן הצדקה' WHERE id=1;
+UPDATE site_settings SET logo_url='/gmach-berega-logo.jpg', tagline='גדולה גמילות חסדים יותר מן הצדקה' WHERE id=1;
 PRAGMA foreign_keys = ON;
